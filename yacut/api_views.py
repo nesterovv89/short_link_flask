3cdad561db419bb0cc2e187a4f0ff543317164f0
Павел Нестеров <necterov.p@gmail.com>
2024-02-16 22:25:16 +0300

    rev2

--- yacut/api_views.py
+++ yacut/api_views.py
@@ -1,40 +1,22 @@
-from re import match
-
 from flask import jsonify, request
 
-from . import app, db
-from .constants import AVAILABLE_SYMBOLS, MAX_SHORT_URL
+from . import app
 from .error_handlers import InvalidAPIUsage
-from .models import URLMap
-from .utils import get_unique_short_id
+from .utils import ShortLinkGenerate
 
 
 @app.route('/api/id/', methods=['POST'])
 def create_short_link():
     data = request.get_json()
-    if not data:
-        raise InvalidAPIUsage('Отсутствует тело запроса')
-    if 'url' not in data:
-        raise InvalidAPIUsage('"url" является обязательным полем!')
-    custom_id = data.get('custom_id', None)
-    if not custom_id or custom_id is None:
-        data['custom_id'] = get_unique_short_id()
-    if len(data['custom_id']) > MAX_SHORT_URL:
-        raise InvalidAPIUsage('Указано недопустимое имя для короткой ссылки')
-    if URLMap.query.filter_by(short=data['custom_id']).first():
-        raise InvalidAPIUsage('Предложенный вариант короткой ссылки уже существует.')
-    elif not match(AVAILABLE_SYMBOLS, data['custom_id']):
-        raise InvalidAPIUsage('Указано недопустимое имя для короткой ссылки')
-    new_url = URLMap()
-    new_url.from_dict(data)
-    db.session.add(new_url)
-    db.session.commit()
-    return jsonify(new_url.to_dict()), 201
+    short_link_service = ShortLinkGenerate()
+    new_url = short_link_service.create_short_link(data)
+    return jsonify(new_url), 201
 
 
 @app.route('/api/id/<short_id>/', methods=['GET'])
 def get_original_link(short_id):
-    link = URLMap.query.filter_by(short=short_id).first()
-    if link is not None:
-        return jsonify({'url': link.original}), 200
+    short_link_service = ShortLinkGenerate()
+    original_link = short_link_service.get_original_link(short_id)
+    if original_link is not None:
+        return jsonify({'url': original_link}), 200
     raise InvalidAPIUsage('Указанный id не найден', 404)
--- yacut/views.py
+++ yacut/views.py
@@ -1,34 +1,34 @@
-from flask import abort, redirect, render_template
+from flask import abort, flash, redirect, render_template
 
-from . import app, db
+from . import app
 from .forms import URLForm
 from .models import URLMap
-from .utils import get_unique_short_id
+from .utils import ShortLinkGenerate
 
 
 @app.route('/', methods=['GET', 'POST'])
 def index_view():
     form = URLForm()
+    short_link_service = ShortLinkGenerate()
+
     if form.validate_on_submit():
-        custom_id = form.custom_id.data
-        if not custom_id:
-            custom_id = get_unique_short_id()
-        elif URLMap.query.filter_by(short=custom_id).first():
-            form.custom_id.errors = ['Предложенный вариант короткой ссылки уже существует.']
+        data = {
+            'url': form.original_link.data,
+            'custom_id': form.custom_id.data,
+        }
+        if URLMap.query.filter_by(short=data['custom_id']).first():
+            flash('Предложенный вариант короткой ссылки уже существует.')
             return render_template('index.html', form=form)
-        url_map = URLMap(
-            original=form.original_link.data,
-            short=custom_id,
-        )
-        db.session.add(url_map)
-        db.session.commit()
-        return (render_template('index.html', form=form, short=custom_id), 200)
+        new_url = short_link_service.create_short_link(data)
+        if new_url:
+            return (render_template('index.html', form=form, short=data['custom_id']), 200)
     return render_template('index.html', form=form)
 
 
 @app.route('/<string:short>', methods=['GET'])
 def redirect_view(short):
-    original_link = URLMap.query.filter_by(short=short).first()
+    short_link_service = ShortLinkGenerate()
+    original_link = short_link_service.get_original_link(short)
     if original_link:
-        return redirect(original_link.original)
+        return redirect(original_link)
     abort(404)
